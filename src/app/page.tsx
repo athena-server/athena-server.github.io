81b60b6f48f48ff83def8bbafa4cc8de691faf3a
th3bossc <th3bossc@users.noreply.github.com>
2024-11-28 20:56:29 +0000

    deploy: bb05ea20b4b061cda9d3b3a1daa2cf09e3a664c4

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-const Page = () => {
-    return (
-        <div className="h-screen w-screen flex items-center justify-center">
-            Athena
-        </div>
-    );
-}
-
-export default Page;
