f3a282acb067670866adc84afd693cfd68e39f10
Vaishnavi <vaishrpai@gmail.com>
2024-12-09 00:18:32 +0300

    Testing out

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        lightBlue: "#39caf0",
+        neonGreen: "#2cff05",
       },
     },
     screens: {
