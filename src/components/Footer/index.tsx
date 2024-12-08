ab6da9c8fcd792ad8bc18e4f00f35f9c32be01c4
Ellvius <lishinvs6541@gmail.com>
2024-12-08 21:08:47 +0530

    improve responsiveness

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -23,15 +23,15 @@ const Footer = ({
 
     return (
         <footer className="px-8 bg-[#202127] border-t border-white md:px-10">
-            <div className="font-bold flex flex-wrap justify-center  items-center py-4 lg:justify-between">
+            <div className="font-bold text-sm flex flex-wrap justify-center  items-center py-5 sm:justify-between md:text-base">
                 {/* Contact Detail */}
-                <div className="flex flex-col mt-2 mb-8 md:flex-row md:space-x-5  md:mt-0  md:mb-0">
+                <div className="flex flex-col mt-2 mb-8  lg:space-x-5  lg:mt-0  lg:mb-0 lg:flex-row">
                     {[
                         {label:email, icon:"/Footer/mail.svg"},
                         {label:phone, icon:"/Footer/phone.svg"},
                         {label:location, icon:"/Footer/location.svg"},
                     ].map((item, index)=>(
-                        <div key={index} className="flex md:justify-center items-center space-x-2 p-2">
+                        <div key={index} className="flex lg:justify-center items-center space-x-2 p-2">
                             <img src={item.icon} alt={item.label} className="md:w-5 md:h-5"/>
                             <p>{item.label}</p>
                         </div>
@@ -41,7 +41,7 @@ const Footer = ({
                 {/* Copyright Section */}
                 <div className="flex justify-center items-center mt-8 mb-6 md:mt-0 md:mb-0">
                     <p className="text-center">
-                        Copyright &copy; {new Date().getFullYear()} CSED, 
+                        Copyright Ⓒ {new Date().getFullYear()} CSED, 
                         {isMobile ? ' NIT Calicut' : ' National Institute of Technology Calicut'}
                     </p>
                 </div>
