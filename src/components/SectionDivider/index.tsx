71ff05cc5e793c22255db3b9982b2e81275eda1c
th3bossc <diljith2003@gmail.com>
2024-12-09 16:46:12 +0400

    added section divider component

---
+++ src/components/SectionDivider/index.tsx
@@ -0,0 +1,14 @@
+/**
+ * Section divider to be used between sections of a page 
+ * 
+ * @returns {JSX.Element} The rendered section divider component.
+ * 
+ * @author Diljith P D
+ */
+const SectionDivider = () => {
+    return (
+        <div className="w-screen max-w-[1920px] absolute top-0 bg-white h-[1px] left-0 sm:left-[-1rem] md:left-[-4rem] 2xl:left-0" />
+    )
+}
+
+export default SectionDivider;
