0067030a8ed0a319c4269ab32a48bde0050e95ab
th3bossc <diljith2003@gmail.com>
2024-12-09 15:21:41 +0400

    moved commits from 1-global-button to 6-button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,10 @@
-"use client";
-
-import Button from "@/components/Button";
 import Container from "@/components/Container";
 
 const Page = () => {
     return (
         <Container>
-            <div className="min-h-screen h-full w-full">
-                <section>
-                    {/* hero section */}
-                </section>
-
-                <section id="about-us">
-
-                </section>
-
-                <section id="facilities" className="flex items-center justify-center mt-16">
-                    <Button onClick={() => console.log("hello")}>
-                        hello
-                    </Button>
-                </section>
-
-                <section id="resources">
-
-                </section>
-
-                <section id="more-links">
-
-                </section>
+            <div className="h-screen w-full flex items-center justify-center">
+                Athena
             </div>
         </Container>
     );
