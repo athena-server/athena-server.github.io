14d8485b74128b61257f1c6d00b146f3a9951237
th3bossc <diljith2003@gmail.com>
2024-12-10 15:14:53 +0400

    added boilerplate code

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,11 +1,33 @@
 import NextLink from 'next/link';
 
-const Link = () => {
-    // return (
-    //     <NextLink >
 
-    //     </NextLink>
-    // )
+/**
+ * link comopnent to be used globally
+ * 
+ * @param {string} [LinkProps.href] - the url to navigate to
+ * @param {string} [LinkProps.label] - the text to display
+ * @param {boolean} [LinkProps.openInNewTab] - whether to open the link in a new tab (defaults to false)
+ * @param {string} [LinkProps.className] - the class name to apply to the link
+ * 
+ * @returns {JSX.Element} - the link component
+ */
+const Link = ({
+    href,
+    label,
+    openInNewTab = false,
+    className = "",
+}: LinkProps): JSX.Element => {
+    return (
+        <NextLink href={href}>
+            {label}
+        </NextLink>
+    )
 }
 
 export default Link;
+export interface LinkProps {
+    href: string;
+    label: string;
+    openInNewTab?: boolean;
+    className?: string;
+}
