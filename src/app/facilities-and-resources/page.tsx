788ee167f1cdfe02fb2a97c2bd44112b1c3a92ed
mihirpa18 <punchithayamihir@gmail.com>
2024-12-10 23:45:14 +0530

    Facilities and resources-page-1

--- src/app/facilities-and-resources/page.tsx
+++ src/app/facilities-and-resources/page.tsx
@@ -1,36 +1,196 @@
+"use client";
+import { useState, useEffect, useMemo } from "react";
 import Container from "@/components/Container";
 import SectionDivider from "@/components/SectionDivider";
-import { alumniSans } from "@/fonts";
-// to use font alumni sans on any text, use the following css: <div className={`${alumniSans.classname} ...tailwind styles`}
-// default font is inter so need to change font for any other elements
-// refer page.tsx in branch 1-home-page for further doubt clearing on any usage of font, or carousal or section divider component
-// navbar and footer are already implemented in the layout component so no need to import them here (they are merged to main so won't be visible in this branch but it'll be there when you merge your branch to main)
-
-// dont edit any component as it can cause issues in other places there those components are used
-// only edit in the section where you are working so as to not conflict with the others working on same page
-// you can create components in the components folder, IF AND ONLY IF you feel those components will be used in multiple parts, and not just in one spot, 
-// before creating a component, ask me to make sure the comopnent doesn't already exist and doesn't cause future conflicts
-
-// delete these comments once you are done with them
+import { alumniSans, inter } from "@/fonts";
 
 const Page = () => {
-    return (
-        <Container>
-            <section className="relative min-h-screen h-full">
-                {/* Section 1 */}
-            </section>
-            <section className="relative min-h-screen h-full">
-                <SectionDivider />
+  const [activeIndex, setActiveIndex] = useState(0);
+  const server = 9;
+  const terminal = 75;
+  const workStation = 2;
+  const networking = 7;
 
-                {/* section 2 */}
-            </section>
-            <section className="relative min-h-screen h-full">
-                <SectionDivider />
+  const carouselData = [
+    { id: 1, text: "FacilitiesPage/image1.png" },
+    { id: 2, text: "FacilitiesPage/image2.png" },
+    { id: 3, text: "FacilitiesPage/image3.png" },
+    { id: 4, text: "FacilitiesPage/image2.png" },
+    { id: 5, text: "FacilitiesPage/image3.png" },
+  ];
 
-                {/* section 3 */}
-            </section>
-        </Container>
-    )
-}
+  const activeImage = useMemo(
+    () => carouselData[activeIndex].text,
+    [activeIndex]
+  );
+  const prevImage = useMemo(
+    () =>
+      carouselData[
+        (activeIndex - 1 + carouselData.length) % carouselData.length
+      ].text,
+    [activeIndex, carouselData]
+  );
+  const nextImage = useMemo(
+    () => carouselData[(activeIndex + 1) % carouselData.length].text,
+    [activeIndex, carouselData]
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveIndex((prevIndex) => (prevIndex + 1) % carouselData.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [carouselData.length]);
+
+  const handleDotClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <Container>
+      <section className="relative min-h-screen h-full">
+        <div className="flex gap-5 flex-col min-h-screen min-w-screen
+         lg:flex-row lg:w-[1256px] lg:h-[642px] lg:top-[209px] lg:left-[128px] mx-5 lg:mx-auto">
+          <div className="mx-auto flex flex-col flex-shrink  lg:w-[60%]">
+            <h1 className={`${alumniSans.className} text-8xl font-bold `}>
+              FACILITIES
+            </h1>
+            <span
+              className={`${inter.className} text-white font-normal text-lg mt-10 mx-auto`}
+            >
+              The lab has high-performance computers with LAN and internet
+              facility through wired connectivity. It also provides WiFi, Audio
+              Visual Projection Systems and printers. Moreover, it is equipped
+              with 9 servers with advanced configurations for central data
+              storage, website hosting, DHCP service, conduction of exams, LDAP
+              service.
+            </span>
+
+            <div className="m-10 pb-0  hidden bg-[#0E0E10]  lg:w-[665px] lg:h-[320px] relative lg:block ">
+              <div className="absolute  lg:w-[455px] lg:h-[259px] lg:top-[33px] border-[1px] border-white">
+                <img className="h-full w-full" src={prevImage} alt="" />
+              </div>
+              <div className="z-10 absolute w-[160px] h-[110px] left-[10px] bottom-[160px] sm:w-[180px] sm:h-[150px] sm:left-[185px] sm:bottom-[110px] lg:top-[5px] lg:w-[500px] lg:h-[300px] lg:left-[79px] border-[1px] border-white flex items-center justify-center">
+                <img
+                  src={activeImage}
+                  alt=""
+                  className="h-full w-full transition-transform duration-500 ease-in-out transform"
+                />
+              </div>
+              <div className="absolute w-[120px] h-[80px] sm:w-[150px] sm:h-[100px] sm:top-[15px] sm:left-[150px] lg:w-[455px] lg:h-[259px] lg:left-[210px] lg:top-[33px] border-[1px] border-white">
+                <img className="h-full w-full" src={nextImage} alt="" />
+              </div>
+
+              <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 flex space-x-2">
+                {Array.from({ length: 4 }).map((_, dotIndex) => {
+                  return (
+                    <div
+                      key={dotIndex}
+                      className={`w-3 h-3 rounded-full cursor-pointer ${
+                        activeIndex === dotIndex
+                          ? "bg-green-500"
+                          : "bg-gray-500 hover:bg-green-500"
+                      }`}
+                      onClick={() => handleDotClick(dotIndex)}
+                    ></div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+
+          <div className="h-auto w-auto md:w-[461px] md:h-[598px] border-[1px] border-white xl:flex-shrink-0 p-4 m-4 mx-auto">
+            <h2
+              className={`${alumniSans.className} text-[#2CFF05] font-bold text-[48px] ml-4`}
+              style={{
+                textShadow: `
+                  0px 0px 19.08px #2CFF05,
+                  0px 0px 38.16px #2CFF05,
+                  0px 0px 133.56px #2CFF05,
+                  0px 0px 267.12px #2CFF05,
+                  0px 0px 457.92px #2CFF05,
+                  0px 0px 801.36px #2CFF05
+                `,
+              }}
+            >
+              Resources
+            </h2>
+            <ul className="text-white mt-4  space-y-6">
+              <li className="gap-4 flex ">
+                <span className="h-[32px] w-[32px]">
+                  <img src="FacilitiesPage/server.png" alt="" />
+                </span>
+                <span
+                  className={`${inter.className} text-xl flex justify-center items-center`}
+                >
+                  {server} High Performing Servers{" "}
+                </span>
+              </li>
+              <li className="gap-4 flex ">
+                <span className="h-[32px] w-[32px]">
+                  <img src="FacilitiesPage/cctv.png" alt="" />
+                </span>
+                <span
+                  className={`${inter.className} text-xl flex justify-center items-center`}
+                >
+                  24 hrs CCTV Monitoring{" "}
+                </span>
+              </li>
+              <li className="gap-4 flex ">
+                <span className="h-[32px] w-[32px]">
+                  <img src="FacilitiesPage/monitor.png" alt="" />
+                </span>
+                <span
+                  className={`${inter.className} text-xl flex justify-center items-center`}
+                >
+                  {terminal} Terminal Systems{" "}
+                </span>
+              </li>
+              <li className="gap-4 flex ">
+                <span className="h-[32px] w-[32px]">
+                  <img src="FacilitiesPage/computer.png" alt="" />
+                </span>
+                <span
+                  className={`${inter.className} text-xl flex justify-center items-center`}
+                >
+                  {workStation} Work Station with GPU{" "}
+                </span>
+              </li>
+              <li className="gap-4 flex ">
+                <span className="h-[32px] w-[32px]">
+                  <img src="FacilitiesPage/networking.png" alt="" />
+                </span>
+                <span
+                  className={`${inter.className} text-xl flex justify-center items-center`}
+                >
+                  {networking} Advanced Networking Equipments{" "}
+                </span>
+              </li>
+              <li className="gap-4 flex ">
+                <span className="h-[32px] w-[32px]">
+                  <img src="FacilitiesPage/wifi.png" alt="" />
+                </span>
+                <span
+                  className={`${inter.className} text-xl flex justify-center items-center`}
+                >
+                  Wi-Fi Facility{" "}
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+
+      <section className="relative min-h-screen h-full">
+        <SectionDivider />
+        {/* section 2 */}
+      </section>
+      <section className="relative min-h-screen h-full">
+        <SectionDivider />
+        {/* section 3 */}
+      </section>
+    </Container>
+  );
+};
 
 export default Page;
