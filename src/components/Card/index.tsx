d849c9c9cfe7eda99f6739fc15981664938a70f9
Nandana-Neo <nandana715tvm@gmail.com>
2024-12-16 01:44:12 +0530

    Changed gradient, name-display, and updated code

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,30 +1,4 @@
-/*
-    * remove these comments once u have read them
-    * the comments are just for guidance
-    * the second comment is jsDocs comment, don't remove those
-    * they are for refernece for the people who are going to use this component
-    * 
-    * use tailwindcss preferably
-    * in case you need modular css, createa `styles.module.css` file within this same folder use similar to how it is being used in src/components/Button component
-    * 
-    * by default the font is inter, so no change is needed
-    * i have imported alumniSansa and geo font in case it is needed. To see how those fonts are being used you can refer the other pages with sections where those fonts are used
-    * 
-    * do not edit any file other than files in this folder
-    * 
-    * to test out the component, use the (debug) folder as it is gitignored
-    * 
-    * don't create any additional components without asking
-    * 
-    * to use images or static files, save the file to "/public/Card/{filename}"
-    * and to use them within the <img /> use src="/Card/{filename}"
-    * 
-    * clarify any doubts before acting on them
-*/
-
-
-
-import { alumniSans, geo } from "@/fonts";
+import { alumniSans } from "@/fonts";
 import styles from "./styles.module.css";
 
 
@@ -45,7 +19,6 @@ const Card = ({
     // designation,
     socials,
 }: CardProps): JSX.Element => {
-    const { email, linkedin, github } = socials || {};
     return (
         <div className="flex flex-col gap-[5px] w-[250px]" >
             <div className="relative flex items-center justify-center w-[250px] h-[250px] bg-neonGreen">
@@ -55,15 +28,24 @@ const Card = ({
                     className="w-full h-full rounded-full object-cover bg-black"
                 />
   
-                <div className="absolute bottom-0 h-1/2 w-full bg-[radial-gradient(circle_at_bottom_left,_#3F56E9_0%,_transparent_35%)] pointer-events-none z-10"></div>
+                <div className="absolute bottom-0 h-1/2 w-full 
+                    bg-[radial-gradient(circle_at_20%_99%,_rgba(63,86,233,1)_0%,_transparent_45%)]
+                    opacity-65
+                    pointer-events-none z-10"/>
+                <div className="absolute bottom-0 h-1/2 w-full 
+                    bg-[radial-gradient(ellipse_at_4%_99%,_rgba(63,86,233,1)_0%,_transparent_18%)]
+                    opacity-40
+                    pointer-events-none z-10"/>
             </div>
 
-            <div className="w-[250px] border-[1px] border-solid border-neonGreen px-[9px] py-[7px] flex flex-col items-start gap-[8px]">
-                <p className={`${alumniSans.className} w-full font-bold text-[32px] leading-[38px]`}>{name}</p>
+            <div className="w-[250px] border-[1px] border-solid border-neonGreen px-[9px] py-[7px] flex flex-col  items-start gap-[8px]">
+                <div className={`${socials===undefined? "place-self-center":""}`}>
+                    <p className={`${alumniSans.className} ${socials===undefined? "text-center":""} font-bold text-[32px]  leading-[38px]`}>{name}</p>
+                </div>
                 {socials && 
                     <div className="flex flex-row gap-[12px] items-center p-0">
                         {socials.email && 
-                            <a href={`mailto:${socials.email}?`} >
+                            <a href={`mailto:${socials.email}`} >
                                 <img src="/Card/email.svg" alt={`icon-email`} className={`${styles.txtshadow}`}/>
                             </a>
                         }
@@ -89,7 +71,6 @@ export default Card;
 export interface CardProps {
     imageUrl: string,
     name: string,
-    // designation: string, // job title
     socials?: {
         email?: string,
         linkedin?: string,
