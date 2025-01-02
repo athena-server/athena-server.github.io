5be34b9fa0da11d6883d19445481b7e800d129db
Vaishnavi <vaishrpai@gmail.com>
2025-01-02 17:58:16 +0530

    Course card done

--- src/components/CourseCard/index.tsx
+++ src/components/CourseCard/index.tsx
@@ -4,8 +4,8 @@ import styles from './styles.module.css';
 /**
 * Cards for the Course Review page
 * @param {string} - Course Title
-* @param {number} - Course ID
-* @param {string} - Review Count
+* @param {string} - Course ID
+* @param {number} - Review Count
 * 
 * @author Vaishnavi R Pai
 */
@@ -15,11 +15,25 @@ const CourseCard = ({
     reviewCount,
 }: CardProps) : JSX.Element => {
     return (
-        <div className="flex flex-col w-[350px] h-[120px] p-[4px_11px] gap-[16px] border-b-[1px] opacity-0"> 
-            <div className="w-[328px] h-[64px] gap-0 opacity-0"> 
-                <p className={`${alumniSans.className} text-[28px] font-bold leading-[32px] text-left underline underline-offset-auto decoration-skip-none`}>
+        <div className="flex flex-col w-[350px] h-[120px] p-[4px_11px] gap-[16px] border-b-[1px]"> 
+            <div className="w-[328px] h-[64px] gap-0"> 
+                <p className={`${alumniSans.className} text-[28px] font-bold leading-[32px] text-white`}>
                     {courseTitle}
                 </p>
+            </div>
+
+            <div className="flex flex-row w-[328px] h-[32px] gap-0 justify-between">
+                <div className="w-[64px] h-[32px] gap-0">
+                    <p className={`${alumniSans.className} text-[24px] font-bold leading-[32px] text-white`}>
+                        {courseId}
+                    </p>
+                </div>
+
+                <div className="w-[78px] h-[32px] gap-0">
+                    <p className={`${alumniSans.className} text-[24px] font-bold leading-[32px] text-white`}>
+                        {reviewCount}
+                    </p>
+                </div>
             </div>
         </div>
     )
@@ -28,6 +42,6 @@ const CourseCard = ({
 export default CourseCard;
 export interface CardProps {
     courseTitle: string,
-    courseId: number,
+    courseId: string,
     reviewCount: string,
 }
