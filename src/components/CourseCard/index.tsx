166c9b5feecd1aea12f4ec81d4e72155f070deef
Vaishnavi <vaishrpai@gmail.com>
2025-01-27 18:33:41 +0530

    Cards checked and finalised

--- src/components/CourseCard/index.tsx
+++ src/components/CourseCard/index.tsx
@@ -1,49 +1,73 @@
+"use client";
+
 import { alumniSans } from "@/fonts";
 import styles from './styles.module.css';
+import { easeInOut, motion } from 'framer-motion'
+import { MouseEventHandler } from "react";
 
 /**
 * Cards for the Course Review page
 * @param {string} [props.courseTitle] - Course Title
 * @param {string} [props.courseId] - Course ID
 * @param {number} [props.reviewCount] - Review Count
-* 
+* @param {MouseEventHandler<HTMLButtonElement>} [props.onClick] - onClick action
+*  
 * @author Vaishnavi R Pai
 */
 const CourseCard = ({ 
     courseTitle,
     courseId,
     reviewCount,
+    onClick = undefined,
 }: CardProps) : JSX.Element => {
     return (
-        <div className={`flex flex-col w-[350px] h-[120px] p-[4px_11px_4px_11px] gap-[16px] border-b-[1px] ${styles.card}`}> 
+        <motion.button
+            layoutId={`${courseId}`}
+            className="flex flex-col w-full xs:w-[350px] h-[120px] py-[4px] px-[11px] gap-4 border-b border-b-px"
+            initial={{
+                color: "rgba(255, 255, 255, 1)",
+                backgroundColor: "rgba(0, 0, 0, 1)",
+                scale: 1,
+            }}
+            whileHover={{
+                color: "rgba(0, 0, 0, 1)",
+                backgroundColor: "rgba(255, 255, 255, 1)",
+                scale: 1.01,
+            }}
+            whileTap={{
+                color: "rgba(0, 0, 0, 1)",
+                backgroundColor: "rgba(255, 255, 255, 1)",
+                scale: 0.99,
+            }}
+            transition={{
+                duration: 0.2,
+                ease: easeInOut,
+            }}
+            onClick={onClick}
+        > 
             <div className="w-[328px] h-[64px]"> 
-                <p className={`${alumniSans.className} w-11/12 text-[28px] font-bold leading-[32px] text-white`}>
+                <h4 className={`${alumniSans.className} w-11/12 text-[28px] font-bold leading-[32px] text-left text-inherit`}>
                     {courseTitle}
-                </p>
+                </h4>
             </div>
 
-            <div className="flex flex-row w-[328px] h-[32px] gap-0 justify-between">
-                <div className="w-[64px]h-[32px]">
-                    <p className={`${alumniSans.className} text-[24px] font-bold leading-[32px] text-white`}>
+            <div className="flex flex-row items-center w-full h-[32px] justify-between">
+                <div className="w-full h-[32px]">
+                    <h5 className={`${alumniSans.className} text-[24px] text-left font-bold leading-[32px] text-inherit`}>
                         {courseId}
-                    </p>
+                    </h5>
                 </div>
 
-                <div className = "flex flex-row gap-[5px] w-[78px] h-[32px]">
-                    <div className="w-[10px]">
-                        <p className={`${alumniSans.className} text-[24px] font-bold leading-[32px] text-white`}>
-                            {reviewCount}
-                        </p>
-                    </div>
-
-                    <div className="w-[65px]">
-                        <p className={`${alumniSans.className} text-[24px] font-bold leading-[32px] text-white`}>
-                            Reviews
-                        </p>
-                    </div>
+                <div className = "flex flex-row gap-[5px] w-full h-[32px] justify-end">
+                    <span className={`${alumniSans.className} text-[24px] font-bold leading-[32px] text-inherit`}>
+                        {reviewCount}
+                    </span>
+                    <span className={`${alumniSans.className} text-[24px] font-bold leading-[32px] text-inherit`}>
+                        Reviews
+                    </span>
                 </div>
             </div>
-        </div>
+        </motion.button>
     )
 };
 
@@ -51,5 +75,6 @@ export default CourseCard;
 export interface CardProps {
     courseTitle: string,
     courseId: string,
-    reviewCount: string,
+    reviewCount: number,
+    onClick?: MouseEventHandler<HTMLButtonElement>
 }
